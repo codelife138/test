6fe0d4be75e611e3519404af9dba90297c5dc61d
Mars1836 <codelife138@gmail.com>
2024-10-14 13:01:47 +0700

    create branch codelife2

--- task3.js
+++ task3.js
@@ -1,5 +1,7 @@
 function task3() {
   console.log("create by codelife1");
+  console.log("create by codelife2");
+  console.log("updated by codelife3");
 }
 
 task3();
---
+++ task4.js
@@ -0,0 +1,5 @@
+function task4() {
+  console.log("create in codelife2");
+}
+
+task4();
